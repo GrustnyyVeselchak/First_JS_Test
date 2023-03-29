e315503a5b3e47bb8df28086a9f0fa07758883d2
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-03-29 18:51:40 +0400

    First JS test is started

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # First_JS_Test
+
+Here is I try to complete tasks from: https://learn.javascript.ru/
---
+++ let.js
@@ -0,0 +1,12 @@
+//Работа с переменными
+let admin;
+let name;
+
+name = "Джон"
+admin = name
+alert(admin);
+
+//Придумайте правильные имена
+
+let planet;
+let userName
