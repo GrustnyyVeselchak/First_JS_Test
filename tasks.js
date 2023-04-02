f15855e166b5e23cf929586fda4690836d3df1cf
GrustnyyVeselchak <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-04-02 13:42:15 +0400

    завершение первого блока

---
+++ tasks.js
@@ -0,0 +1,19 @@
+//Работа с переменными
+let admin;
+let name;
+
+name = "Джон"
+admin = name
+alert(admin);
+
+//Придумайте правильные имена
+
+let planet;
+let userName
+
+//Взаимодействие: alert, prompt, confirm
+
+userName = prompt('Как тебя зовут?', []); //вызывает модальное окно с тектосвым полем
+confirm(`Тебя зовут ${userName}?`); // вызывает модальное коно с выбором ок\отмена
+alert(userName);
+
