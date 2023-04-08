88f2977f9ee819ab6a7bb4ae7e56bd22ca7c5a30
GrustnyyVeselchak <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-04-08 15:50:30 +0400

    Закончил второй блок

--- tasks.js
+++ tasks.js
@@ -1,3 +1,4 @@
+/*
 //Работа с переменными
 let admin;
 let name;
@@ -24,3 +25,54 @@ let b = prompt("Второе число?", 2);
 
 alert(a + b); // 12
 alert(+a + +b); //3
+
+
+//Инструкция "if"
+
+if ("") {
+    alert( 'Привет' );
+  } else {
+    alert( 'Пока');
+  };
+
+title = prompt('Какое "официальное" название JavaScritp?');
+
+(title === "ECMAScript") ? alert("Верно") : alert ("Не знаете? ECMAScript!");
+
+value = prompt ()
+if (value>0) {
+    alert(1);
+}   else if (value<0) {
+    alert (-1);
+}   else if (value==0) {
+    alert (0);
+}
+
+
+let message;
+
+(login == 'Сотрудник') ? message = 'Привет':
+(login == 'Директор') ? message = 'Здравствуйте':
+(login == 'Нет логина') ? message = 'Нет логина': message = '';
+
+*/
+
+//Логические операторы 
+
+let login = prompt('Введите логин');
+let password;
+
+if (login ==='Админ') {
+    password = prompt('Введите пароль')
+    if (password === 'Я главный') {
+        alert('Hello');
+    }   else if (password === '' || password === null) {
+        alert('Отменено')
+    }   else {
+        alert('Неверный пароль');
+    }
+}   else if (login === '' || login === null) {
+    alert('Отменено');
+}   else {
+    alert('Я вас не знаю');
+}   
